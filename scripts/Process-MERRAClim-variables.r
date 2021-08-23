c787f25144a1159057533085bb881229e8d864e9
Tanner C. Myers <tannercmyers@gmail.com>
2021-08-23 14:35:46 -0500

    Using 10m resolution instead of 2.5m

--- scripts/Process-MERRAClim-variables.r
+++ scripts/Process-MERRAClim-variables.r
@@ -21,29 +21,33 @@
 
 #----------------------------Downloading & unzipping bioclimatic variables----------------------------#
         ## https://datadryad.org/stash/dataset/doi:10.5061/dryad.s2v81 is the MERRAclim data link
-    ## download url for MerraClim
-    data_url <- https://datadryad.org/stash/downloads/file_stream/96972" # 2000s means @ 2.5 arc-minutes
+    # download url for MerraClim
+    data_url <- "https://datadryad.org/stash/downloads/file_stream/96972" # 2000s means @ 2.5 arc-minutes
     
-    ## download data
+    # download data
     download.file(data_url, destfile = "10m_mean_00s.zip", method = "auto", 
               quiet = FALSE, mode = "wb", cacheOK = TRUE)
 
-    ## unzip variables
-    var_dir <- "10m_mean_00s"
-    dir.create(var_dir)
-    unzip(zipfile = "10m_mean_00s.zip", exdir=var_dir)
+    # unzip variables
+    var_dir <- "data/10m_mean_00s"
+        ## create directory to deposit files in & unzip 
+        dir.create(var_dir)
+        unzip(zipfile = "10m_mean_00s.zip", exdir=var_dir)
+    
+    # delete zip file
+    file.remove("10m_mean_00s.zip")
 #-----------------------------------------------------------------------------------------------------#
 
 #--------------------------------------Preparing variables--------------------------------------------#
-    ## stacking of variables (excluding 8,9,18, and 19; see Escobar et al. (2014))
-    mc2_5 <- stack(list.files("2_5m_mean_00s", pattern = "bio.*if$", full.names = T))
-    names(mc2_5) <- gsub("X2_5m_mean_00s_", "", names(mc2_5))
-    mc2_5 <- mc2_5[[c(1, 12:17, 2:9)]]
+    # stack variables (excluding 8,9,18, and 19; see Escobar et al. (2014))
+    mc10 <- stack(list.files("10m_mean_00s", pattern = "bio.*if$", full.names = T))
+    names(mc10) <- gsub("X10m_mean_00s_", "", names(mc10))
+    mc10 <- mc10[[c(1, 12:17, 2:9)]]
 
         ## At 2.5 arc-minutes, this is a lot of data to deal with and I am not interested in projecting
         ## the suitability of habitats outside of Hispaniola so I am going to crop both the raster & shapefile loaded below
         limits <- extent(c(-76,-67, 17, 21))
-        mc2_5_subset <- crop(mc2_5, limits)
+        mc10_subset <- crop(mc10, limits)
 
     # Shapefile featuring country boundaries from: https://hub.arcgis.com/datasets/252471276c9941729543be8789e06e12_0/data
     world <- readOGR(dsn = ".", "World_Countries__Generalized_")
@@ -53,15 +57,15 @@
     nhisp <- gUnaryUnion(hisp, hisp@data$id)
 
     # Getting the cells with values 
-    cells_nona <- cellFromPolygon(mc2_5_subset[[1]], nhisp )
-    cellsids <- 1:ncell(mc2_5_subset[[1]])
+    cells_nona <- cellFromPolygon(mc10_subset[[1]], nhisp )
+    cellsids <- 1:ncell(mc10_subset[[1]])
     cell_na <- cellsids[-cells_nona[[1]]]
-    mc2_5_subset[cell_na] <- NA
-    plot(mc2_5_subset[[1]])
+    mc10_subset[cell_na] <- NA
+    plot(mc10_subset[[1]])
 
     # Saving variables to a new directory
     dir.create("data/merra_new")
 
-    lapply(names(mc2_5_subset), function(x) {
-        writeRaster(mc2_5_subset[[x]], paste0("data/merra_new/", x, ".asc"), overwrite = TRUE)
+    lapply(names(mc10_subset), function(x) {
+        writeRaster(mc10_subset[[x]], paste0("data/merra_new/", x, ".asc"), overwrite = TRUE)
     })
