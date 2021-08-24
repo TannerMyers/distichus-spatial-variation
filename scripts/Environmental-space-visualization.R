e51e63bf0861e15f95ce7d66aebc1d19ecadbad0
Tanner C. Myers <tannercmyers@gmail.com>
2021-08-24 13:59:01 -0500

    Re-ordering code in script for better flow

--- scripts/Environmental-space-visualization.R
+++ scripts/Environmental-space-visualization.R
@@ -1,13 +1,30 @@
-setwd("~/Dropbox/Distichus_Project/distichus_spatial_morphological_variation/")
+
+
+# Directories
+working_dir <- getwd()
+setwd(working_dir)
 
 # Load data for individuals that have been measured 
-data <- read.csv("complete-data.csv", header=TRUE)
+data <- read_csv("complete-data.csv")
+str(data)
+head(data)
+
+     ## Designate population colors for plotting
+     data$Color[data$Subspecies=="aurifer"] <-"peru" 
+     data$Color[data$Subspecies=="vinosus"] <-"firebrick3" 
+     data$Color[data$Subspecies=="suppar"] <-"purple1" 
+     data$Color[data$Subspecies=="favillarum"] <-"dodgerblue" 
+     data$Color[data$Subspecies=="ravitergum"] <-"yellow" 
+     data$Color[data$Subspecies=="dominicensis"] <-"bisque" 
+     data$Color[data$Subspecies=="ignigularis"] <-"darkorange2" 
+     data$Color[data$Subspecies=="properus"] <-"seagreen3"   
+
 columns <- c("Subspecies", "Longitude", "Latitude", "Color")
 
-df <- data[, columns]
+df <- data[,columns]
 
 # Load processed MERRAClim bioclimatic variables at 2.5 arc seconds resolution
-bioclim <- stack(list.files(path="merra_new", pattern = ".asc", full.names = T))
+bioclim <- stack(list.files(path="data/merra_new", pattern = ".asc", full.names = T))
 
 # Extract raster values for the localities in `df`
 bc_values <- raster::extract(bioclim,data.frame(df$Longitude, df$Latitude))
@@ -25,39 +42,15 @@ leg.txt <- c(expression(italic("ignigularis")),
              expression(italic("vinosus")))
 
 pdf("MERRAclim-environmental-biplot.pdf")
+#hgd()
+#hgd_browse()
 plot(df$bio12 ~ df$bio1, 
      col=df$Color, pch=16, cex=1.25,
      xlab="Annual Mean Temperature", ylab="Annual Precipitation",
-     main="Environmental Space (MERRAClim)")
+     main="Environmental Space (MERRAClim -- 10m 2000s Mean)")
 legend("topleft", leg.txt, col=unique(data$Color), pch =16,
        cex=1)
 dev.off()
-
-
-data <- read.csv("complete-data.csv", header=TRUE)
-  head(data)
-  str(data)
-
-  unique(data$Subspecies)
-# Levels: aurifer dominicensis favillarum ignigularis properus ravitergum suppar vinosus
-  
-  data$Color[data$Subspecies=="aurifer"] <-"peru" 
-  data$Color[data$Subspecies=="vinosus"] <-"firebrick3" 
-  data$Color[data$Subspecies=="suppar"] <-"purple1" 
-  data$Color[data$Subspecies=="favillarum"] <-"dodgerblue" 
-  data$Color[data$Subspecies=="ravitergum"] <-"yellow" 
-  data$Color[data$Subspecies=="dominicensis"] <-"bisque" 
-  data$Color[data$Subspecies=="ignigularis"] <-"darkorange2" 
-  data$Color[data$Subspecies=="properus"] <-"seagreen3"   
-
-  leg.txt <- c(expression(italic("ignigularis")),
-               expression(italic("ravitergum")),
-               expression(italic("favillarum")),
-               expression(italic("properus")),
-               expression(italic("dominicensis")),
-               expression(italic("aurifer")),
-               expression(italic("suppar")),
-               expression(italic("vinosus")))
   
   pdf("Worldclim-environmental-biplot.pdf")
   plot(data$bio12_23 ~ data$bio1_23, 
