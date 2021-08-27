b4a9bf9a9b19dbb3bdb441f1c5d6ddbcf07d4139
Tanner C. Myers <tannercmyers@gmail.com>
2021-08-27 12:26:13 -0500

    Updating path names

--- scripts/Process-MODIS-vegetation-data.r
+++ scripts/Process-MODIS-vegetation-data.r
@@ -13,6 +13,10 @@
 
 ###############
 
+# directories 
+working_dir <- getwd()
+setwd(working_dir)
+
 # load packages
 library(raster)
 library(rgdal)
@@ -21,13 +25,13 @@ library(tidyverse)
 
 # Load rasters for May vegetation indices
     ## Files should have '121' following the year. This is the Julian date for May 1st
-may_evi <- raster::stack(list.files(path="May", pattern="EVI", full.names = TRUE, recursive = TRUE))
-may_ndvi <- raster::stack(list.files(path="May", pattern="NDVI", full.names = TRUE, recursive = TRUE))
+may_evi <- raster::stack(list.files(path="data/MODIS/May", pattern="EVI", full.names = TRUE, recursive = TRUE))
+may_ndvi <- raster::stack(list.files(path="data/MODIS/May", pattern="NDVI", full.names = TRUE, recursive = TRUE))
 
 # Load rasters for March vegetation indices
     ## Files should have '060' following the year. This is the Julian date for March 1st
-march_evi <- raster::stack(list.files(path="March", pattern="EVI", full.names = TRUE, recursive = TRUE))
-march_ndvi <- raster::stack(list.files(path="March", pattern="NDVI", full.names = TRUE, recursive = TRUE))
+march_evi <- raster::stack(list.files(path="data/MODIS/March", pattern="EVI", full.names = TRUE, recursive = TRUE))
+march_ndvi <- raster::stack(list.files(path="data/MODIS/March", pattern="NDVI", full.names = TRUE, recursive = TRUE))
 
 
 
@@ -38,8 +42,8 @@ march_evi_mean <- raster::calc(march_evi, mean)
 march_ndvi_mean <- raster::calc(march_ndvi, mean)
 
 # Save mean rasters to files
-writeRaster(x = march_ndvi_mean, filename =file.path("March/march_NDVI_mean"), format = "ascii", overwrite = TRUE)
-writeRaster(x = may_ndvi_mean, filename = file.path("May/may_NDVI_mean"), format = "ascii", overwrite = TRUE)
-writeRaster(x = march_evi_mean, filename = file.path("March/march_EVI_mean"), format = "ascii", overwrite = TRUE)
-writeRaster(x = may_evi_mean, filename = file.path("May/may_EVI_mean"), format = "ascii", overwrite = TRUE)
+writeRaster(x = march_ndvi_mean, filename =file.path("data/MODIS/March/march_NDVI_mean"), format = "ascii", overwrite = TRUE)
+writeRaster(x = may_ndvi_mean, filename = file.path("data/MODIS/May/may_NDVI_mean"), format = "ascii", overwrite = TRUE)
+writeRaster(x = march_evi_mean, filename = file.path("data/MODIS/March/march_EVI_mean"), format = "ascii", overwrite = TRUE)
+writeRaster(x = may_evi_mean, filename = file.path("data/MODIS/May/may_EVI_mean"), format = "ascii", overwrite = TRUE)
 
