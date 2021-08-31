660e7478c557b3fa5dd5a74de52fa9ec3df3c530
Tanner C. Myers <tannercmyers@gmail.com>
2021-08-31 13:10:33 -0500

    Removing elevation download and processing components to put in the Process-environmental-variables.r script

--- scripts/Raster-PCA.r
+++ scripts/Raster-PCA.r
@@ -13,18 +13,7 @@ library(tidyverse)
 
 bc_list <- raster::stack(list.files("data/chelsa_new/", pattern=".asc", full.names = TRUE))
 vi_list <- raster::stack(list.files("data/MODIS_new/", pattern=".asc", full.names = TRUE))
-
-## Include elevation among other environmental variables
-    # dir.create("data/elevation")
-    # raster::getData(name="SRTM", path="data/elevation", lon = -74, lat=18)
-    # raster::getData(name="SRTM", path="data/elevation", lon = -69, lat=18)
-    # raster::getData(name="SRTM", path="data/elevation", lon = -74, lat=21)
-alt1 <- raster("data/elevation/srtm_22_09.tif")
-alt2 <- raster("data/elevation/srtm_23_09.tif")
-alt3 <- raster("data/elevation/srtm_22_08.tif")
-## Need to merge rasters because they represent different tiles from the SRTM
-## dataset
-elev <- merge(alt,alt2,alt3)
+elev <- raster("data/elevation_new/SRTM_elevation_1km.asc")
 
     ## Rasters have different extents
         extent(bc_list)
@@ -45,20 +34,9 @@ elev <- merge(alt,alt2,alt3)
     ## still not quite the same
     bc_list@extent <- raster::alignExtent(bc_list@extent, vi_list)
 
-    elev <- crop(elev, vi_list@extent)
-    elev@extent <- raster::alignExtent(elev@extent, vi_list) 
-
-    ## we need to resample the elevation raster because it has 
-    ## 10x as many rows and columns than CHELSA and MODIS variables
-        ## SRTM is at ~90 m resolution, while CHELSA and MODIS are ~1 km
-    elev_resampled <- resample(elev, vi_list)
-
-
 ## Now, merge resolved raster stacks 
-env <- raster::stack(elev_resampled, bc_list, vi_list, full.names=TRUE)
+env <- raster::stack(elev, bc_list, vi_list, full.names=TRUE)
 plot(env)
-
-# 
 
 ## perform principal component analysis on rasters
 pca <- rasterPCA(env, spca=TRUE)
