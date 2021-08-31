245022f7d9475ebf4bcfb0b9cef9fb574b1141d4
Tanner C. Myers <tannercmyers@gmail.com>
2021-08-31 13:47:17 -0500

    Moving adjustment of CHELSA rasters to Create-M script prior to masking variables by M

--- scripts/Raster-PCA.r
+++ scripts/Raster-PCA.r
@@ -10,32 +10,8 @@ library(rgdal)
 library(RStoolbox)
 library(tidyverse)
 
-
-bc_list <- raster::stack(list.files("data/chelsa_new/", pattern=".asc", full.names = TRUE))
-vi_list <- raster::stack(list.files("data/MODIS_new/", pattern=".asc", full.names = TRUE))
-elev <- raster("data/elevation_new/SRTM_elevation_1km.asc")
-
-    ## Rasters have different extents
-        extent(bc_list)
-        # class      : Extent 
-        # xmin       : -76.00014 
-        # xmax       : -67.00014 
-        # ymin       : 16.99986 
-        # ymax       : 20.99986 
-        extent(vi_list)
-        # class      : Extent 
-        # xmin       : -74.81667 
-        # xmax       : -68.16667 
-        # ymin       : 17.4 
-        # ymax       : 20.81667 
-    ## Since the extent of vi_list fits within bc_list's extent, let's
-    ## crop bc_list to make it the same for both
-    bc_list <- crop(bc_list, vi_list@extent)
-    ## still not quite the same
-    bc_list@extent <- raster::alignExtent(bc_list@extent, vi_list)
-
-## Now, merge resolved raster stacks 
-env <- raster::stack(elev, bc_list, vi_list, full.names=TRUE)
+## load environmental variables as a raster stack 
+env <- raster::stack(list.files())
 plot(env)
 
 ## perform principal component analysis on rasters
