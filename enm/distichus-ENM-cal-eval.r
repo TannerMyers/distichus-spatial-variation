456f588eb10193c41cf247c8203539214a6e6e76
Tanner C. Myers <tannercmyers@gmail.com>
2021-10-11 13:03:56 -0500

    Changing script to use "sample with data" approach.
    Instead of drawing from the raster files, the SWD approach instead
    extracts values from each raster for every occurrence and then samples
    the rasters for background points. The .csv files produced from this
    will be used to fit Maxent models with kuenm.

--- enm/distichus-ENM-cal-eval.r
+++ enm/distichus-ENM-cal-eval.r
@@ -4,15 +4,28 @@ setwd(working_dir)
 
 library(kuenm)
 
-#prepare_swd(occ=jointPoints, species="species", longitude="longitude", latitude="latitude", 
-#            data.split.method = "random",
-#            train.proportion = 0.5, raster.layers=vars, sample.size = 30000,
-#            var.sets = set, save = T, name.occ="occurrences",
-#            back.folder="M_2", set.seed = 1)
+occ_joint <- "Adist1_all.csv" # All occurrences
+jointPoints <- read.csv(occ_joint)
+
+## load seven environmental variables identified as non-correlated
+variables1 <- raster::stack(list.files(path="M_variables_test/Set_99", pattern=".asc$", full.names=TRUE))
+## load first 5 principal components output by "Raster-PCA.r" script that account for 96.4% of the variation 
+variables2 <- raster::stack(list.files(path="M_variables_test/PC_set", pattern=".asc$", full.names=TRUE))
+## merge into single stack
+variables <- raster::stack(variables1, variables2) 
+
+
+set <- list(Set_1=c(names(variables1)), Set_2=c(names(variables2)))
+
+prepare_swd(occ=jointPoints, species="species", longitude="longitude", latitude="latitude", 
+            data.split.method = "random",
+            train.proportion = 0.5, raster.layers=variables2, sample.size = 30000,
+            var.sets = set, save = T, name.occ="occurrences",
+            back.folder="M_2", set.seed = 1)
 
 # Assign variables
-occ_joint <- "Adist1_all.csv" # All occurrences
-occ_tra <- "Adist1_train.csv" # training occurrences to be used for calibration
+occ_joint <- "occurrences_joint.csv"
+occ_tra <- "occurrences_train.csv"
 M_var_dir <- "M_variables_test"
 batch_cal <- "Candidate_models"
 out_dir <- "Candidate_Models"
