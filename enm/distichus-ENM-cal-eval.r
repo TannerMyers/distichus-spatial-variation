e68381b1ce458c636bee448c0aafe4eecf8e0831
Tanner C. Myers <tannercmyers@gmail.com>
2021-10-14 11:04:24 -0400

    Commenting out unnecessary lines of code and adding lines to run kuenm_mod_swd

--- enm/distichus-ENM-cal-eval.r
+++ enm/distichus-ENM-cal-eval.r
@@ -4,23 +4,23 @@ setwd(working_dir)
 
 library(kuenm)
 
-occ_joint <- "Adist1_all.csv" # All occurrences
-jointPoints <- read.csv(occ_joint)
+# occ_joint <- "Adist1_all.csv" # All occurrences
+# jointPoints <- read.csv(occ_joint)
 
 ## load seven environmental variables identified as non-correlated
 ## CHELSA_bio10_03.asc CHELSA_bio10_04.asc CHELSA_bio10_05.asc CHELSA_bio10_15.asc CHELSA_bio10_16.asc march_EVI_mean.asc may_NDVI_mean.asc
-variables <- raster::stack(list.files(path="M_variables_test/Set_99", pattern=".asc$", full.names=TRUE)) 
+# variables <- raster::stack(list.files(path="M_variables_test/Set_99", pattern=".asc$", full.names=TRUE)) 
 
-set <- list(Set_1=c(names(variables)), Set_2=c("CHELSA_bio10_03", "CHELSA_bio10_04", "CHELSA_bio10_15", "march_EVI_mean", "may_NDVI_mean"), Set_3=c("CHELSA_bio10_05", "CHELSA_bio10_16","march_EVI_mean", "may_NDVI_mean"),
-	   Set_4=c("CHELSA_bio10_04","CHELSA_bio10_16","may_NDVI_mean"))
+# set <- list(Set_1=c(names(variables)), Set_2=c("CHELSA_bio10_03", "CHELSA_bio10_04", "CHELSA_bio10_15", "march_EVI_mean", "may_NDVI_mean"), Set_3=c("CHELSA_bio10_05", "CHELSA_bio10_16","march_EVI_mean", "may_NDVI_mean"),
+# 	   Set_4=c("CHELSA_bio10_04","CHELSA_bio10_16","may_NDVI_mean"))
 
-prepare_swd(occ=jointPoints, species="species", longitude="longitude", latitude="latitude", 
-            data.split.method = "random",
-            train.proportion = 0.5, raster.layers=variables, sample.size = 30000,
-            var.sets = set, save = T, name.occ="occurrences",
-            back.folder="M_2", set.seed = 1)
+# prepare_swd(occ=jointPoints, species="species", longitude="longitude", latitude="latitude", 
+#             data.split.method = "random",
+#             train.proportion = 0.5, raster.layers=variables, sample.size = 30000,
+#             var.sets = set, save = T, name.occ="occurrences",
+#             back.folder="M_2", set.seed = 1)
 
-# Assign variables
+# Assign variables needed for generating candidate models
 occ_joint <- "occurrences_joint.csv"
 occ_tra <- "occurrences_train.csv"
 occ_test <- "occurrences_test.csv"
@@ -39,8 +39,30 @@ run <- TRUE
 ## the need to test broad suites of parameter combinations, such as, distinct regularization multiplier values, 
 ## various feature classes, and different sets of environmental variables. 
 ## The following code calls the help page of the function kuenm_cal.
-kuenm_cal_swd(occ.joint = occ_joint, occ.tra = occ_tra, occ.test = occ_test, 
-	  back.dir = back_dir, batch = batch_cal,
-          out.dir.models = out_dir, out.dir.eval = out_dir_eval,
-          reg.mult = reg_mult, f.clas = f_clas, args = args, max.memory = 1000,
-          maxent.path = maxent_path, selection = "OR_AICc", kept = FALSE)
+# kuenm_cal_swd(occ.joint = occ_joint, occ.tra = occ_tra, occ.test = occ_test, 
+# 	  back.dir = back_dir, batch = batch_cal,
+#           out.dir.models = out_dir, out.dir.eval = out_dir_eval,
+#           reg.mult = reg_mult, f.clas = f_clas, args = args, max.memory = 1000,
+#           maxent.path = maxent_path, selection = "OR_AICc", kept = FALSE)
+
+# Assign variables needed for generating final models
+rep_n <- 10
+rep_type <- "Bootstrap"
+jackknife <- FALSE
+out_format <- "logistic"
+project <- TRUE
+G_var_dir <- "G"
+ext_type <- "all"
+write_mess <- FALSE
+write_clamp <- FALSE
+wait <- FALSE
+run <- TRUE
+args <- NULL
+out.dir <- "mod_swd"
+
+kuenm_mod_swd(occ_joint, back_dir, out_dir_eval, batch, rep_n,
+              rep_type, jackknife,
+              max.memory = 1000, out_format,
+              project, G_var_dir, ext_type,
+              write_mess, write_clamp, maxent_path,
+              args, out.dir, wait, run)
