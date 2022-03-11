5269bb099b623a566c1e8fa3f855f0964588762a
Tanner Myers <tannercmyers@gmail.com>
2022-03-11 10:52:37 -0600

    Update kuenm-cal-eval.r
    Adding kuenm_cal_swd command to do model generation and evaluation while commenting out the kuenm_cal and kuenm_ceval functions that are only used in the case in which environmental data is supplied as raster layers.

--- scripts/kuenm-cal-eval.r
+++ scripts/kuenm-cal-eval.r
@@ -28,6 +28,7 @@ occ_joint <- "occurrences_joint.csv"
 occ_tra <- "occurrences_train.csv"
 occ_test <- "occurrences_test.csv"
 M_var_dir <- "M_variables"
+back_dir <- "M_2"
 batch_cal <- "Candidate_models"
 out_dir <- "Candidate_Models"
 reg_mult <- c(seq(0.1, 1, 0.1), seq(2, 6, 1), 8, 10)
@@ -36,10 +37,11 @@ args <- NULL
 maxent_path <- "/home/tcm0036/distichus-spatial-variation/enm"
 wait <- FALSE
 run <- TRUE
+kept <- FALSE
 
-kuenm_cal(occ.joint = occ_joint, occ.tra = occ_tra, M.var.dir = M_var_dir, batch = batch_cal,
-          out.dir = out_dir, reg.mult = reg_mult, f.clas = f_clas, args = args,
-          maxent.path = maxent_path, wait = wait, run = run)
+#kuenm_cal(occ.joint = occ_joint, occ.tra = occ_tra, M.var.dir = M_var_dir, batch = batch_cal,
+#          out.dir = out_dir, reg.mult = reg_mult, f.clas = f_clas, args = args,
+#          maxent.path = maxent_path, wait = wait, run = run)
 
 out_eval <- "Calibration_results"
 threshold <- 5
@@ -49,7 +51,14 @@ kept <- TRUE
 selection <- "OR_AICc"
 paral_proc <- FALSE
 
-cal_eval <- kuenm_ceval(path = out_dir, occ.joint = occ_joint, occ.tra = occ_tra, occ.test = occ_test,
-                        batch = batch_cal, out.eval = out_eval, threshold = threshold,
-                        rand.percent = rand_percent, iterations = iterations, kept = kept,
-                        selection = selection, parallel.proc = paral_proc)
+#cal_eval <- kuenm_ceval(path = out_dir, occ.joint = occ_joint, occ.tra = occ_tra, occ.test = occ_test,
+#                        batch = batch_cal, out.eval = out_eval, threshold = threshold,
+#                        rand.percent = rand_percent, iterations = iterations, kept = kept,
+#                        selection = selection, parallel.proc = paral_proc)
+
+# The function `kuenm_cal_swd` does the entirety of model calibration and evaluation, unlike cases
+# in which environmental data is supplied as rasters that executes two functions
+kuenm_cal_swd(occ.join = occ_joint, occ.tra = occ_tra, occ.test = occ_test,
+             back.dir = back_dir, batch = batch_cal, out.dir.models = out_dir,
+             out.dir.eval = out_eval, reg.mult = reg_mult, f.clas = f_clas,
+             args = args, maxent.path = maxent_path, selection = selection, kept = kept)
