3ff0784b62c1c827c27d1a128f50af672a2db6bf
Tanner C. Myers <tannercmyers@gmail.com>
2022-10-21 16:54:13 -0400

    Fixing errors

--- scripts/rangebreak-tests.r
+++ scripts/rangebreak-tests.r
@@ -1,10 +1,11 @@
 
+library(tidyverse)
 library(raster)
 library(rgdal)
 library(rgeos)
 library(ENMTools)
 
-setwd("/home/tcm0036/distichus-ddRAD/distichus-spatial-variation")
+setwd("/home/tcm0036/distichus-spatial-variation")
 
 # Load filtered occurrences downloaded with `spocc` R package
 occs_ad <- read_csv("data/occurrences/filtered_distichus_occurrences.csv")
@@ -43,35 +44,35 @@ K5 <- enmtools.species(species.name = "K5", presence.points = read.csv("niche-as
     check.species(K5)
 
 ## Eastern A. d. dominicensis vs western A. d. dominicensis and South paleo-island
-K1K2.rbb <- rangebreak.blob(species.1 = K1, species.2 = K2, type = "mx", nreps = 1000)
+K1K2.rbb <- rangebreak.blob(species.1 = K1, species.2 = K2, env = env2, type = "mx", nreps = 1000)
     png("niche-assessment/K_1_2_rbb.png")
         plot(K1K2.rbb)
     dev.off()
     save(K1K2.rbb, file = "niche-assessment/K1_K2.rbb")
 
 ## Eastern A. d. dominicensis vs A. d. ignigularis
-K1K3.rbb <- rangebreak.blob(species.1 = K1, species.2 = K3, type = "mx", nreps = 1000) 
+K1K3.rbb <- rangebreak.blob(species.1 = K1, species.2 = K3, env = env2, type = "mx", nreps = 1000) 
     png("niche-assessment/K_1_3_rbb.png")
         plot(K1K3.rbb)
     dev.off()
     save(K1K3.rbb, file = "niche-assessment/K1_K3.rbb")
 
 ## A. d. ignigularis vs A. d. properus
-K3K5.rbb <- rangebreak.blob(species.1 = K3, species.2 = K5, type = "mx", nreps = 1000)
+K3K5.rbb <- rangebreak.blob(species.1 = K3, species.2 = K5, env = env2, type = "mx", nreps = 1000)
     png("niche-assessment/K_3_5_rbb.png")
         plot(K3K5.rbb)
     dev.off()
     save(K3K5.rbb, file = "niche-assessment/K3K5.rbb")
 
 ## A. d. ignigularis vs A. d. ravitergum
-K3K4.rbb <- rangebreak.blob(species.1 = K3, species.2 = K4, type = "mx", nreps = 1000)
+K3K4.rbb <- rangebreak.blob(species.1 = K3, species.2 = K4, env = env2, type = "mx", nreps = 1000)
     png("niche-assessment/K_3_4_rbb.png")
         plot(K3K4.rbb)
     dev.off()
     save(K3K4.rbb, file = "niche-assessment/K3K4.rbb")
 
 ## A. d. ravitergum vs western A. d. dominicensis and South paleo-island
-K4K2.rbb <- rangebreak.blob(species.1 = K4, species.2 = K2, type = "mx", nreps = 1000)
+K4K2.rbb <- rangebreak.blob(species.1 = K4, species.2 = K2, env = env2, type = "mx", nreps = 1000)
     png("niche-assessment/K_4_2_rbb.png")
         plot(K4K2.rbb)
     dev.off()
