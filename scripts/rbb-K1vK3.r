589b0be445d2bdb5c508fe26bf8bd8f0df7fd681
Tanner C. Myers <tannercmyers@gmail.com>
2022-10-24 08:22:34 -0500

    Adding individual scripts for each pair of lineages to be compared with rangebreak tests.

---
+++ scripts/rbb-K1vK3.r
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(raster)
+library(rgdal)
+library(rgeos)
+library(ENMTools)
+
+setwd("/home/tcm0036/distichus-spatial-variation")
+
+# Load environmental variables
+env <- raster::stack(list.files(path = "data/env_imputed", pattern = ".asc", full.names = TRUE))    
+env2 <- env[[c("CHELSA_bio10_03", "CHELSA_bio10_04",
+            "CHELSA_bio10_15", "CHELSA_bio10_16",
+            "march_EVI_mean", "may_NDVI_mean")]]
+    ENMTools::check.env(env2)
+
+# Create ENMTools species objects
+K1 <- enmtools.species(species.name = "K1", presence.points = read.csv("niche-assessment/K_1_thinned.csv"))
+    K1$range <- background.raster.buffer(K1$presence.points, radius = 50000, mask = env2[[1]])
+    K1$background.points <- background.buffer(points = K1$presence.points, buffer.width = 20000, buffer.type = "convhull", return.type = "points", n = 3000, mask = env2[[1]])
+    check.species(K1)
+
+K3 <- enmtools.species(species.name = "K3", presence.points = read.csv("niche-assessment/K_3_thinned.csv"))
+    K3$range <- background.raster.buffer(K3$presence.points, radius = 50000, mask = env2[[1]])
+    K3$background.points <-background.buffer(points = K3$presence.points, buffer.width = 20000, buffer.type = "convhull", return.type = "points", n = 3000, mask = env2[[1]])
+    check.species(K3)
+
+####################################################################################################
+# Run rangebreak tests
+## Eastern A. d. dominicensis vs A. d. ignigularis
+K1K3.rbb <- rangebreak.blob(species.1 = K1, species.2 = K3, env = env2, type = "mx", nreps = 1000) 
+    png("niche-assessment/K_1_3_rbb.png")
+        plot(K1K3.rbb)
+    dev.off()
+    print(summary(K1K3.rbb))
+    save(K1K3.rbb, file = "niche-assessment/K1K3.rbb.RData")
